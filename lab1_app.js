3382707a66bbeb48d8c63a7ed55c1d9e93489193
RetardWorks <61974688+RetardWorks@users.noreply.github.com>
2020-05-05 18:26:46 +0200

    Update lab1_app.js

--- lab1_app.js
+++ lab1_app.js
@@ -1,36 +1,60 @@
 //Laboration 1
-const express = require('express');
+
+'use strict';
+
+const express = require( 'express' );
 const bodyParser = require('body-parser');
 const jsDOM = require('jsdom');
 const fs = require('fs');
 const blogPosts = require('./blogPosts');
+
 let app = express();
-let sectionRef;
-let serverDOM;
-app.listen(3000,function(){
-    console.log('server is running on port 3000');
-});
-app.get('/',function(request,response){
-    response.sendFile(__dirname + '/index.html');
-    let htmlCode = fs.readFileSync(__dirname +'/index.html');
-    serverDOM = new jsDOM.JSDOM(htmlCode);
-    sectionRef = serverDOM.window.document.querySelector('section');
-    /*let postNode =  serverDOM.window.document.createTextNode(blogPosts.blogPosts[0].nickName
-        + ' '+ blogPosts.blogPosts[0].msgSubject +' ' + blogPosts.blogPosts[0].timeStamp + 
-        ' '+blogPosts.blogPosts[0].msgBody
-    );*/
-    let postNode;
-    for(let i =0;i<blogPosts.blogPosts.length;i++){
-        postNode =serverDOM.window.document.createTextNode(blogPosts.blogPosts[i].nickName
-            + ' '+ blogPosts.blogPosts[i].msgSubject +' ' + blogPosts.blogPosts[i].timeStamp + 
-            ' '+blogPosts.blogPosts[i].msgBody
-        );
-    }
-    sectionRef.appendChild(postNode);
-    htmlCode = serverDOM.serialize();
-    response.send(htmlCode);
+let serverDom;
+
+app.listen('81', function() {
+    console.log('now runs port 81!');
 });
 
-app.get('/skriv',function(request,response){
-   response.sendFile(__dirname + '/skriv.html');
+app.get('/', function( request, response) {
+    response.sendFile( __dirname + '/index.html' );
+
+    let htmlCode = fs.readFileSync(__dirname + '/index.html');
+    let serverDom = new jsDOM.JSDOM(htmlCode);
+
+    let sectionRef = serverDom.window.document.querySelector('section');
+
+    let postNode;
+    for(let i=0;i<blogPosts.blogPosts.length;i++) {
+        postNode = serverDom.window.document.createTextNode(blogPosts.blogPosts[i].nickname + ' ' +
+    blogPosts.blogPosts[i].msgSubject + ' ' + blogPosts.blogPosts[i].timeStamp + ' ' +
+    blogPosts.blogPosts[i].msgBody);
+    }
+    sectionRef.appendChild(postNode);
+    htmlCode = serverDom.serialize();
+    response.send(htmlCode);
+
 });
+
+app.get('/skriv', function(request, response){
+    response.sendFile(__dirname + '/skriv.html');
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/skriv', function(request, response) {
+    let htmlCode = fs.readFileSync(__dirname + '/index.html');
+    let serverDom = new jsDOM.JSDOM(htmlCode);
+    let bodyNodeRef = serverDom.window.document.querySelector('body');
+
+    let divNodeRef;
+    let divTextNodeRef;
+
+    let text = request.body.subject;
+    let textarea = request.body.msgBody;
+    let nickname = request.body.nickname;
+
+    for(let i = 1;i<=nickname;i++) {
+        
+    }
+});
+
