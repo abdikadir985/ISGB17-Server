6b0384c206d64f60c8a8cccb9ba6c8ee57cc5ed4
oehqoehfo <hosap4ik@gmail.com>
2020-05-06 17:06:48 +0200

    Update lab1_app.js

--- lab1_app.js
+++ lab1_app.js
@@ -1,60 +1,61 @@
 //Laboration 1
-
-'use strict';
-
-const express = require( 'express' );
+const express = require('express');
 const bodyParser = require('body-parser');
 const jsDOM = require('jsdom');
 const fs = require('fs');
 const blogPosts = require('./blogPosts');
-
 let app = express();
-let serverDom;
+let sectionRef;
+let serverDOM;
 
-app.listen('81', function() {
-    console.log('now runs port 81!');
+app.listen(3000,function(){
+    console.log('server is running on port 3000');
 });
-
-app.get('/', function( request, response) {
-    response.sendFile( __dirname + '/index.html' );
-
-    let htmlCode = fs.readFileSync(__dirname + '/index.html');
-    let serverDom = new jsDOM.JSDOM(htmlCode);
-
-    let sectionRef = serverDom.window.document.querySelector('section');
-
-    let postNode;
-    for(let i=0;i<blogPosts.blogPosts.length;i++) {
-        postNode = serverDom.window.document.createTextNode(blogPosts.blogPosts[i].nickname + ' ' +
-    blogPosts.blogPosts[i].msgSubject + ' ' + blogPosts.blogPosts[i].timeStamp + ' ' +
-    blogPosts.blogPosts[i].msgBody);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/public',express.static(__dirname+'/public'));
+app.get('/',function(request,response){
+    response.sendFile(__dirname + '/index.html');
+    let htmlCode = fs.readFileSync(__dirname +'/index.html');
+    serverDOM = new jsDOM.JSDOM(htmlCode);
+    sectionRef = serverDOM.window.document.querySelector('section');
+    let postNode='', postTextNode;
+    console.log('just for checking');
+    for(let i =0;i<blogPosts.blogPosts.length;i++){
+       /* postNode = serverDOM.window.document.createTextNode(blogPosts.blogPosts[i].msgSubject + ' '+ 
+        blogPosts.blogPosts[i].timeStamp +' ' + blogPosts.blogPosts[i].msgBody + ' '+ 
+        blogPosts.blogPosts[i].nickName
+        );*/
+        postNode = postNode + '<article><span>'+blogPosts.blogPosts[i].msgSubject + '</span> ' + 
+        blogPosts.blogPosts[i].timeStamp +' <div>' + blogPosts.blogPosts[i].msgBody + '</div><span> '+ 
+        blogPosts.blogPosts[i].nickName+'</span></article>';
     }
-    sectionRef.appendChild(postNode);
-    htmlCode = serverDom.serialize();
+    sectionRef.innerHTML=postNode;
+    htmlCode = serverDOM.serialize();
     response.send(htmlCode);
-
 });
 
-app.get('/skriv', function(request, response){
-    response.sendFile(__dirname + '/skriv.html');
+app.get('/skriv',function(request,response){
+   response.sendFile(__dirname + '/skriv.html');
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post('/skriv', function(request, response) {
-    let htmlCode = fs.readFileSync(__dirname + '/index.html');
-    let serverDom = new jsDOM.JSDOM(htmlCode);
-    let bodyNodeRef = serverDom.window.document.querySelector('body');
-
-    let divNodeRef;
-    let divTextNodeRef;
-
-    let text = request.body.subject;
-    let textarea = request.body.msgBody;
+app.post('/skriv',function(request,response){
+    let subject = request.body.subject;
+    let textarea = request.body.msgbody;
     let nickname = request.body.nickname;
-
-    for(let i = 1;i<=nickname;i++) {
-        
+    if(subject.length<3 || textarea.length<10 || nickname.length<3){
+        response.redirect('/skriv');
     }
-});
+    const dateObject = new Date();
+    const year = dateObject.getFullYear();
+    const month = dateObject.getMonth()+1;
+    const date = dateObject.getDate();
+    const dateInText = year+'-'+month+'-'+date;
 
+    blogPosts.blogPosts.push({
+        nickName:nickname,
+        msgSubject:textarea,
+        msgBody:textarea,
+        timeStamp:dateInText
+    });
+    response.redirect('/');
+});
